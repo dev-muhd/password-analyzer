85c36b7aadb9fe230afbbf65ed84f6a6fc368812
dev_muhd <princeharmydex@gmail.com>
2026-02-19 09:02:37 +0100

    feat: add minimum length password check

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,2 +1,5 @@
 def analyze_password(password):
-    print("Analyzing password...")
+    if len(password) < 8:
+        print("Password too short")
+    else:
+        print("Password length is acceptable")
