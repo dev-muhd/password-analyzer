deb85cd03de9b69a4dd5406feb6aa73785c10513
dev_muhd <princeharmydex@gmail.com>
2026-02-20 15:08:08 +0100

    feat: add digit presence password rule

--- src/main.py
+++ src/main.py
@@ -2,3 +2,5 @@ from analyzer import analyze_password
 
 password = input("Enter password: ")
 analyze_password(password)
+score, failures = analyze_password(password)
+print(f"Password score: {score}, Failures: {failures}")
