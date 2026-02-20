8dad054a47c1ca1c3f6defa73db024eb6ddc637c
dev_muhd <princeharmydex@gmail.com>
2026-02-20 15:09:29 +0100

    feat: add digit presence password rule

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,5 +1,8 @@
 def check_min_length(password):
     return len(password) >= 8
+
+def check_has_digit(password):
+    return any(char.isdigit() for char in password)
     
 
 
@@ -10,5 +13,10 @@ def analyze_password(password):
         score += 1
     else:
         failures.append("Password too short (minimum 8 characters)")
+
+    if check_has_digit(password):
+        score += 1
+    else:
+        failures.append("Password must contain at least one digit")
 
     return score, failures
